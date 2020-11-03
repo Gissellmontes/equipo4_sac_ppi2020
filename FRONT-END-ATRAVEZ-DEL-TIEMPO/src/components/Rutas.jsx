3832a175e33c35a62af4fb11defb79f98227e535
Gissellmontes <gimopa2004@gmail.com>
2020-11-03 18:56:18 -0500

    inicio al front

---
+++ FRONT-END-ATRAVEZ-DEL-TIEMPO/src/components/Rutas.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Layout from "./Layout";
+import Home from "../paginas/Home";
+import Iniciosesion from "../paginas/InicioSesion";
+import Registro from "../paginas/Registro";
+import Quiz from "../paginas/Quiz";
+
+function Rutas() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/iniciarsesion" component={Iniciosesion} />
+          <Route exact path="/registro" component={Registro} />
+          <Route exact path="/Quiz" component={Quiz} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default Rutas;
---
+++ FRONT-END-ATRAVEZ-DEL-TIEMPO/src/paginas/Preguntas.jsx
@@ -0,0 +1,32 @@
+export const Preguntas = [
+  {
+    id: 0,
+    pregunta: " Que es un Facebook ?",
+    opciones: [
+      "es una red que conecta personas con personas.",
+      "una cuenta",
+      "un programa"
+    ],
+    respuesta: "es una red que conecta personas con personas."
+  },
+  {
+    id: 1,
+    pregunta: " Que es Instagram?",
+    opciones: [
+      "es una red social y una aplicación móvil",
+      "efectos fotográficos",
+      " tomar fotografías"
+    ],
+    respuesta: "es una red social y una aplicación móvil"
+  },
+  {
+    id: 2,
+    pregunta: "Que es Youtube?",
+    opciones: [
+      `videojuegos`,
+      `es un sitio Web que permite a sus usuarios subir vídeos`,
+      ` es una especie de televisión a la carta por Internet.`
+    ],
+    respuesta: `es un sitio Web que permite a sus usuarios subir vídeos`
+  }
+];
